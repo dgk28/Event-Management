5fec3f3571e88ebbccdc7e6a1f05bd3e0b6d634d
DEVAL G KUMAR <102786172+dgk28@users.noreply.github.com>
2024-10-20 22:40:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the Event Management API!');
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 
